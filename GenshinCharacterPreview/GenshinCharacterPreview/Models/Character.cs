6b2f6ae72d4d4d38e4f500a42de47fff65d58155
ScorpiusT1 <2087346232@qq.com>
2023-08-06 20:11:53 +0800

    first commit

---
+++ GenshinCharacterPreview/GenshinCharacterPreview/Models/Character.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenshinCharacterPreview.Models
+{
+    public class Character
+    {      
+        public string Name { get; init; }
+        public string IconUrl { get; init; }
+        public string ProtraitUrl { get; init; }
+        public string NameUrl { get; set; }
+        public string ElementUrl { get; set; }
+        public string DialogueUrl { get; set; }
+
+        public Character()
+        {
+
+        }
+    }
+}
---
+++ GenshinCharacterPreview/GenshinCharacterPreview/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using GenshinCharacterPreview.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenshinCharacterPreview.ViewModels
+{
+    public partial class MainWindowViewModel:ObservableObject
+    {
+        [ObservableProperty]
+        public Character character = new();
+
+        [ObservableProperty]
+        Character selectedItem;
+
+        [RelayCommand]
+        private void LoadCity()
+        {
+
+        }
+    }
+}
