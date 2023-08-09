8cd109b30cee4536b829421a15b5b13351577b52
ScorpiusT1 <2087346232@qq.com>
2023-08-09 22:52:26 +0800

    second commit, update character preview

--- GenshinCharacterPreview/GenshinCharacterPreview/ViewModels/MainWindowViewModel.cs
+++ GenshinCharacterPreview/GenshinCharacterPreview/ViewModels/MainWindowViewModel.cs
@@ -4,6 +4,7 @@ using GenshinCharacterPreview.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,9 +19,15 @@ namespace GenshinCharacterPreview.ViewModels
         Character selectedItem;
 
         [RelayCommand]
-        private void LoadCity()
+        private async Task LoadCity(int id)
         {
+            string url = $"https://content-static.mihoyo.com/content/ysCn/getContentList?pageSize=20&pageNum=1&order=asc&channelId={id}";
 
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            string content = await response.Content.ReadAsStringAsync();
         }
     }
 }
